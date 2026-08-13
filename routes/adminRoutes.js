a2be78a5cd8fc7ffc90972e7612128fa688416ad
dhavan1029-cmky <dhavan1029@gmail.com>
2026-08-13 20:06:32 +0530

    creating initial admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import userModel from '../models/userModel.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
 router.get('/admin/login', async (req, res) => { 
+    
     res.render('admin/login', {error: '', formData: {}})
+
 })
 
 export default router
