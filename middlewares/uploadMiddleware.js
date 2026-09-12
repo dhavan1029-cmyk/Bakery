f499762c03a93cc92dc9bbb73218cff0a848dd2b
dhavan1029-cmky <dhavan1029@gmail.com>
2026-09-12 19:37:30 +0530

    deleting multer-storage-cloudinary

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,17 +1,8 @@
 import multer from 'multer';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import cloudinary from '../config/cloudinary.js';
-
-const storage = new CloudinaryStorage({
-    cloudinary,
-    params: {
-        folder: 'flourish/products',
-        allowed_formats: ['jpg', 'jpeg', 'png', 'webp']
-    }
-});
 
 const upload = multer({
-    storage
+    dest: "uploads/"
 });
 
 export default upload;
+
